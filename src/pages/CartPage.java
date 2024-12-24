1570b69478795c2cbbab2cb0380bf89088e09999
akshay9698 <akshaykudale3@gmail.com>
2024-12-24 16:53:26 +0530

    created new pages

---
+++ src/pages/CartPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class CartPage {
+	
+	public void cartPage() {
+		System.out.println("this is cart page ");
+	}
+
+
+}
---
+++ src/pages/HomePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class HomePage {
+	
+	public void homePage() {
+		System.out.println("this is home page ");
+	}
+
+}
